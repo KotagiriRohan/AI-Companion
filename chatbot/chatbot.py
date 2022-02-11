942c189d122b8e093c5f6201b4decaf5065a182e
Rohan Kotagiri <rohankotagiri10@gmail.com>
2022-02-11 22:27:57 +0530

    Update chatbot.py

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,1 +1,8 @@
+import transformers
 
+nlp = transformers.pipeline("conversational", model="microsoft/DialoGPT-medium")
+
+for i in range(5):
+    text = input("user -->")
+    chat = nlp(transformers.Conversation(text), pad_token_id=50256)
+    print('bot -->', str(chat))
